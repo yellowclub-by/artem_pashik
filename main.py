63d0350a79a1e3027c4d845807b97fafad89cdcb
yellowclub-by <hello@yellowclub.by>
2025-04-19 19:45:46 +0300

    CreateEchoBot

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-print("Hello world")
+import asyncio
+from aiogram.filters import CommandStart
+from aiogram import Bot, Dispatcher, types
+token = "7574376008:AAFp8i62jg6xMdaI4P-rDW-oGYLH8UbBhJw"
+bot = Bot(token=token)
+dp = Dispatcher()
+@dp.message(CommandStart())
+async def start_cmd(message):
+    await message.answer("Ку, короч он делает то, что ты делаешь. Короч ты пон, бб.")
 
+@dp.message()
+async def echo(message: types.Message):
+    # await message.answer("Lox")
+    user_text = message.text
+    await message.answer(user_text)
+async def main():
+    print("Бот запущен")
+    await dp.start_polling(bot)
 
-
-
-
-
-
-
+asyncio.run(main())
 
 
 
