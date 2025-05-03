f9044a508f924c429e8bbc5d62fe129b603b5763
yellowclub-by <hello@yellowclub.by>
2025-05-03 19:44:33 +0300

    add magic filters

--- Handlers/user_command.py
+++ Handlers/user_command.py
@@ -1,17 +1,19 @@
 import asyncio
 from aiogram.filters import CommandStart, Command
-from aiogram import types, Router
+from aiogram import types, Router, F
 
 user_router = Router()
 
 @user_router.message(CommandStart())
 async def start_cmd(message):
-    await message.answer("Бот для покупки игр. Команды /buy - купить по жанру игры, /сatalog - выбрать из всего каталога")
+    await message.answer("Бот для покупки игр. Команды купить, /buy - купить по жанру игры, каталог, /сatalog - выбрать из всего каталога")
 
+@user_router.message(F.text.lower() == "купить")
 @user_router.message(Command("buy"))
 async def genre(message: types.Message):
     await message.answer("Выберите жанр игры")
 
+@user_router.message(F.text.lower() == "каталог")
 @user_router.message(Command("catalog"))
 async def catalog(message: types.Message):
     await message.answer("Выберите игру из всех доступных")
@@ -56,8 +58,15 @@ async def catalog(message: types.Message):
 # async def katalog(message: types.Message):
 #     await message.answer("Выберите игру из списка возможных:")
 
-@user_router.message()
-async def echo(message: types.Message):
+#@user_router.message(F.text.lower() == "rr")
+#@user_router.message(F.text)
+#@user_router.message(F.photo)
+#@user_router.message(F.text.lower().contains("да"))
+#@user_router.message(F.text.lower().startswith("ку"))
+#@user_router.message(F.text.lower().endswith("бб"))
+# @user_router.message(F.text.lower().startswith("ку"), F.text.lower().endswith("бб"))
+#@user_router.message(F.text.lower().startswith("ку") | F.text.lower().startswith("ук"))
+#async def echo(message: types.Message):
     # await message.answer("Lox")
-    user_text = message.text
-    await message.answer(user_text)
+    #user_text = message.text
+    #await message.answer(user_text)
